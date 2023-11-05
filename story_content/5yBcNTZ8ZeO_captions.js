685dcde4d6003ffecdbe6e191d7a350777787b7f
tanmayk1994 <37327400+tanmayk1994@users.noreply.github.com>
2023-11-05 18:11:12 +0530

    Adding first commit
    First commit

---
+++ story_content/5yBcNTZ8ZeO_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.10.22406.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:06.368%0D%0AFill%20in%20the%20blanks:%20In%20_______%20dementia,%20damage%20to%20the%20brain%20is%20usually%20focused%20on%20the%20front%20part.%0D%0A%0D%0A"}')
---
+++ story_content/6FedRMgTTlb_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.10.22406.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:02.228%0D%0AClick%20%3Ci%3EExit%3C/i%3E,%20to%20exit%20the%20course.%0D%0A%0D%0A"}')
